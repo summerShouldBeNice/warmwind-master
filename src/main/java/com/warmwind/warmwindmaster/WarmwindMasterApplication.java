d7ead6e3b6bfbaeb0791fbd8b1dc5337c1f90426
邱俊峰 <2693387413@qq.com>
2024-05-29 14:13:19 +0800

    初始化项目

---
+++ src/main/java/com/warmwind/warmwindmaster/WarmwindMasterApplication.java
@@ -0,0 +1,13 @@
+package com.warmwind.warmwindmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmwindMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarmwindMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/warmwind/warmwindmaster/WarmwindMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warmwind.warmwindmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmwindMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
